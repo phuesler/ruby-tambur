10ac748a97cf1b9c7a6d8022aece8f5b0a88e964
Patrick Huesler <patrick.huesler@gmail.com>
2012-02-18 21:49:35 +0100

    reformat to ruby 2 space indent format

--- lib/ruby-tambur.rb
+++ lib/ruby-tambur.rb
@@ -4,31 +4,31 @@ require 'net/http'
 require 'openssl'
 
 module Tambur
-    class Connector
-        def initialize(api_key, app_id, secret, options = {})
-            @app_id = app_id
-            @api_host = options[:api_host] || 'api.tambur.io'
-            @oauth = Tambur::Util::OAuth.new()
-            @oauth.consumer_key=api_key
-            @oauth.consumer_secret=secret
-            @oauth.req_method='POST'
-        end
+  class Connector
+    def initialize(api_key, app_id, secret, options = {})
+      @app_id = app_id
+      @api_host = options[:api_host] || 'api.tambur.io'
+      @oauth = Tambur::Util::OAuth.new()
+      @oauth.consumer_key=api_key
+      @oauth.consumer_secret=secret
+      @oauth.req_method='POST'
+    end
 
-        def publish(stream, message)
-            path = '/app/' + @app_id + '/stream/' + stream.to_str+'?api_version=1.0&message='+ CGI::escape(message)
-            url = URI.parse('http://' + @api_host + path)
-            Net::HTTP.start(url.host) { | http |
-                request = Net::HTTP::Post.new(url.path)
-            request.body = @oauth.sign(url).query_string()
-            request['Content-Type'] = 'application/x-www-form-urlencoded'
-            response = http.request(request)
-            if response.code == '204'
-                return true
-            else
-                raise 'publish error: '+response.read_body
-            end
-            }
+    def publish(stream, message)
+      path = '/app/' + @app_id + '/stream/' + stream.to_str+'?api_version=1.0&message='+ CGI::escape(message)
+      url = URI.parse('http://' + @api_host + path)
+      Net::HTTP.start(url.host) do | http |
+        request = Net::HTTP::Post.new(url.path)
+        request.body = @oauth.sign(url).query_string()
+        request['Content-Type'] = 'application/x-www-form-urlencoded'
+        response = http.request(request)
+        if response.code == '204'
+          return true
+        else
+          raise 'publish error: '+response.read_body
         end
+      end
+    end
 
     def generate_auth_token(stream, subscriber_id)
       stream.insert(0,"auth:") unless stream.start_with? "auth:"
